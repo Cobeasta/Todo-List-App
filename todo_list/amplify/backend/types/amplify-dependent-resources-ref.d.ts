3cdd6a6cb539ddbbcfbdb8e46808f317f386d8b3
Cobeasta <stopyloknok@gmail.com>
2024-03-05 14:55:45 -0600

    add amplify cli config

---
+++ todo_list/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "todoliste9844549": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
