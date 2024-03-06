11c19dc736888b4ed1a1eeaf40f5295931ad0f2f
Cobeasta <stopyloknok@gmail.com>
2024-03-05 18:49:36 -0600

    fixing runing?

---
+++ todo_list/lib/amplifyconfiguration.dart
@@ -0,0 +1,51 @@
+const amplifyconfig = '''{
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-2:f49a7107-6b9a-40ed-b065-ed2ca1b7b36b",
+                            "Region": "us-east-2"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-2_vrJYzauXx",
+                        "AppClientId": "6apjv764it8446pn99acn5mo77",
+                        "Region": "us-east-2"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH",
+                        "socialProviders": [],
+                        "usernameAttributes": [],
+                        "signupAttributes": [
+                            "EMAIL"
+                        ],
+                        "passwordProtectionSettings": {
+                            "passwordPolicyMinLength": 8,
+                            "passwordPolicyCharacters": []
+                        },
+                        "mfaConfiguration": "OFF",
+                        "mfaTypes": [
+                            "SMS"
+                        ],
+                        "verificationMechanisms": [
+                            "EMAIL"
+                        ]
+                    }
+                }
+            }
+        }
+    }
+}''';
--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_list/Settings.dart';
+import 'package:todo_list/amplifyconfiguration.dart';
 import 'package:todo_list/database/tables/Task.dart';
 import 'package:todo_list/database/AppDatabase.dart';
 import 'package:todo_list/task/TaskRepository.dart';
@@ -11,10 +15,20 @@ final getIt = GetIt.instance;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  _configureAmplify();
   registerSingletons();
   runApp(const MyApp());
 }
 
+Future<void> _configureAmplify() async {
+  try {
+    final auth = AmplifyAuthCognito();
+    await Amplify.addPlugin(auth);
+    await Amplify.configure(amplifyconfig);
+  } on Exception catch (e) {
+    safePrint("An error occurred configuring Amplify: $e");
+  }
+}
 void registerSingletons() {
   // shared prefs
   getIt.registerSingletonAsync<Settings>(() async {
@@ -35,20 +49,29 @@ void registerSingletons() {
   // view model for a task
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+
   // This widget is the root of your application.
+
+}
+class _MyAppState extends State<MyApp> {
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white10,
-        brightness: Brightness.dark),
-        useMaterial3: true,
-      ),
-      home: const TaskList(),
-    );
+    return Authenticator(
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.white10,
+                brightness: Brightness.dark),
+            useMaterial3: true,
+          ),
+          home: const TaskList(),
+        ));
+
   }
 }
