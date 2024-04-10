fb059d676e4f85a0869167a1bc10f1364cd74859
Cobeasta <stopyloknok@gmail.com>
2024-04-09 19:10:59 -0500

    build script, gitignore

--- todo_list/lib/database/app_database.dart
+++ todo_list/lib/database/app_database.dart
@@ -5,7 +5,7 @@ import 'package:todo_list/database/tables/task.dart';
 import 'package:sqflite/sqflite.dart' as sqflite;
 import 'package:todo_list/database/typeConverters/date_time_converter.dart';
 
-part 'AppDatabase.g.dart'; // the generated code will be there
+part 'app_database.g.dart'; // the generated code will be there
 
 @TypeConverters([DateTimeConverter, OptionalDateTimeConverter])
 @Database(version: 1, entities: [Task])
