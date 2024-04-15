a1897b250a18f80832f396c92c81842edff9d4fc
Cobeasta <stopyloknok@gmail.com>
2024-04-15 12:14:16 -0500

    stuff'

--- todo_list/lib/task/editTask/edit_task_vm.dart
+++ todo_list/lib/task/editTask/edit_task_vm.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/date_utils.dart';
 import 'package:todo_list/main.dart';
 import 'package:todo_list/task/task_model.dart';
 import 'package:todo_list/task/task_repository.dart';
@@ -21,8 +22,11 @@ class EditTaskVM extends ChangeNotifier {
   TextEditingController get descriptionController => _descriptionController;
 
   bool get isCompleted => _taskModel.isComplete;
+
   String get title => _taskModel.title;
+
   String get description => _taskModel.description;
+
   DateTime get deadline => _taskModel.deadline;
 
   void init() {
@@ -43,9 +47,25 @@ class EditTaskVM extends ChangeNotifier {
     });
   }
 
-  void updateTitle() {
-    _taskModel.updateTitle(_titleController.text);
-    notifyListeners();
+  void titleControllerUpdate(String text) {
+    bool notifyFlag = false;
+    var textSplit = text.split("\\s");
+    String newTitle = "";
+    for (int i = 0; i < textSplit.length; i++) {
+      if (textSplit[i].toLowerCase() == "today" ||
+          textSplit[i].toLowerCase() == "tod") {
+        _taskModel.updateDeadline(TaskListDateUtils.today());
+        notifyFlag = true;
+      } else {
+        newTitle = "$newTitle ${textSplit[i].trim()}".trim();
+      }
+    }
+    if (newTitle != title) {
+      _taskModel.updateTitle(newTitle);
+      notifyFlag = true;
+    }
+
+    if (notifyFlag) notifyListeners();
   }
 
   void updateDescription() {
@@ -79,3 +99,5 @@ class EditTaskVM extends ChangeNotifier {
     Navigator.pop(context, _taskModel);
   }
 }
+
+
